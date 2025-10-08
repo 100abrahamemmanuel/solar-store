5daa5794defbe5d3a21abdbfd1530cfeff4f79cd
100abrahamemmanuel <ewelaabraham2007@gmail.com>
2025-10-08 10:28:28 -0700

    git

--- controllers/order.js
+++ controllers/order.js
@@ -8,75 +8,71 @@ const { checkPermissions } = require('../utils');
 
 
 const createOrder = async (req, res) => {
-  const { items: cartItems, tax, shippingFee } = req.body;
+  const { items: cartItems, shippingFee } = req.body;
 
   if (!cartItems || cartItems.length < 1) {
     throw new CustomError.BadRequestError('No cart items provided');
   }
-  if (!tax || !shippingFee) {
-    throw new CustomError.BadRequestError(
-      'Please provide tax and shipping fee'
-    );
+  if (!shippingFee) {
+    throw new CustomError.BadRequestError('Please provide shipping fee');
   }
 
   let orderItems = [];
   let subtotal = 0;
 
   for (const item of cartItems) {
-    const dbProduct = await Product.findOne({ _id: item.product });
-    if (!dbProduct) {
-      throw new CustomError.NotFoundError(
-        `No product with id : ${item.product}`
-      );
+    const product = await Product.findById(item);
+    if (!product) {
+      throw new CustomError.NotFoundError(`No product found with this id: ${item.product}`);
     }
-    const { name, price, image, _id } = dbProduct;
+
     const singleOrderItem = {
+      name: product.title,
+      image: product.image[0]?.url || '', // fallback if image array is empty
+      price: product.price,
       amount: item.amount,
-      name,
-      price,
-      image,
-      product: _id,
+      product: product._id,
     };
-    // add item to order
-    orderItems = [...orderItems, singleOrderItem];
-    // calculate subtotal
-    subtotal += item.amount * price;
+
+    orderItems.push(singleOrderItem);
+    subtotal += item.amount * product.price;
   }
-  // calculate total
-  const total = tax + shippingFee + subtotal;
-  // get client secret
-  const paymentIntent = await fakeStripeAPI({
-    amount: total,
-    currency: 'usd',
-  });
+
+  const total =shippingFee + subtotal;
+
+
 
   const order = await Order.create({
     orderItems,
     total,
     subtotal,
-    tax,
     shippingFee,
-    clientSecret: paymentIntent.client_secret,
+    refreshToken: paymentIntent.client_secret,
     user: req.user.userId,
   });
 
-  res
-    .status(StatusCodes.CREATED)
-    .json({ order, clientSecret: order.clientSecret });
+  res.status(StatusCodes.CREATED).json({
+    order,
+  });
 };
+
 const getAllOrders = async (req, res) => {
   const orders = await Order.find({});
   res.status(StatusCodes.OK).json({ orders, count: orders.length });
 };    
+
+
+
 const getSingleOrder = async (req, res) => {
   const { id: orderId } = req.params;
   const order = await Order.findOne({ _id: orderId });
-  if (!order) {
-    throw new CustomError.NotFoundError(`No order with id : ${orderId}`);
+  if (!order) { 
+    throw new CustomError.NotFoundError(`No order with id : ${orderId}, pls check again`);
   }   
   checkPermissions(req.user, order.user);
   res.status(StatusCodes.OK).json({ order });
 };
+
 const getCurrentUserOrders = async (req, res) => {
   const orders = await Order.find({ user: req.user.userId });
   res.status(StatusCodes.OK).json({ orders, count: orders.length });
@@ -84,17 +80,15 @@ const getCurrentUserOrders = async (req, res) => {
 const updateOrder = async (req, res) => {
   const { id: orderId } = req.params;
   const { paymentIntentId } = req.body;
-
   const order = await Order.findOne({ _id: orderId });
   if (!order) {
     throw new CustomError.NotFoundError(`No order with id : ${orderId}`);
   }
   checkPermissions(req.user, order.user);
-
   order.paymentIntentId = paymentIntentId;
   order.status = 'paid';
   await order.save();
-
+  
   res.status(StatusCodes.OK).json({ order });
 };
 
